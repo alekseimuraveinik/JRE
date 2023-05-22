940690a94dbc62866fc88b7f34ae4e0494dfb52d
Aleksei Muraveinik <aleksei.muraveinik@gmail.com>
2023-05-22 17:56:56 +0400

    2.8

--- Package.swift
+++ Package.swift
@@ -6,9 +6,16 @@ import PackageDescription
 let package = Package(
     name: "JRE",
     products: [
-        
+        .library(
+            name: "JRE",
+            targets: ["JRE"]
+        )
     ],
     targets: [
-        
+        .binaryTarget(
+            name: "JRE",
+            url: "https://github.com/alekseimuraveinik/JRE/releases/download/JRE.xcframework/JRE.xcframework.zip",
+            checksum: "7697bc745425cc0035f9b2cef4187d27cbe88ea66a6abb0f2ed79a5579be845a"
+        )
     ]
 )
